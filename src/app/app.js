e6576eca269e9665760af7ddcd9e4119b8a6a2f5
Ignacio Gómez Puga <i.gomezp@outlook.com>
2022-11-16 21:38:10 -0600

    Se agrega configuracion inicial del servidor

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,15 @@
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello Yorch\n')
-  })
-  
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-  })
+// iGomezP
+const http = require( 'http' );
 
-  // IgomezP
+const port = 8080;
+const hostname = '127.0.0.1'
+
+const server = http.createServer( ( req, res ) => {
+  res.statusCode = 200
+  res.setHeader( 'Content-Type', 'text/plain' )
+  res.end( 'Hello Yorch\n' )
+} )
+
+server.listen( port, hostname, () => {
+  console.log( `Server running at http://${ hostname }:${ port }/` )
+} )
