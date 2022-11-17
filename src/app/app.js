e245f992546e5f87d5bd4f5aa284d7def5e49fc7
IvanGarcia <ivan.garciafou@gmail.com>
2022-11-16 20:56:49 -0600

    Cambios de Profesor

--- src/app/app.js
+++
@@ -1,9 +0,0 @@
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello Yorch\n')
-  })
-  
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-  })
