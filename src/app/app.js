53361fad0f9c606a7ddfa5b2ec3268d6a73df067
Emmanuel Acevedo Diaz <eacevedod@uaemex.mx>
2022-11-16 21:20:19 -0600

    Se documenta el archivo app.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,11 @@
+
+/*Se crea el servidor web y se responde con el codigo 200 y saludo*/
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello Yorch\n')
   })
-  
+ /*Se escucha por el puerto 80*/ 
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
   })
