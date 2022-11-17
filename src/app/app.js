39f1b7c9063f74d7e87642302b61feccc5878bd5
Pablo Perez <pablo.prezparedes@outlook.com>
2022-11-16 21:37:26 -0600

    comments en el script

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,9 @@
+// #Libreria Para inicializar el webserver
 const http = require("http")
+// Host para la conexion 0.0.0.0 para conectarse desde cualquier lugar
+const hostname = '0.0.0.0'
+// Puerto de escucha
+const port = 8080
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
