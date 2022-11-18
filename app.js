b4e5f52142f9889a00839544e60fe4567b966e77
Héctor Mora <h.hector.mora@gmail.com>
2022-11-17 19:08:50 -0800

    Cambios diarios, traducción

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const hostname = 'localhost';
 const server = http.createServer( (req, res) => {
 	res.statusCode = 200;
 	res.setHeader( 'Content-Type', 'text/plain');
-	res.end('Hello Hector\n');
+	res.end('Hola Hector\n');
 });
 
 server.listen( port, hostname, () => {
-	console.log( 'Server running at http://' + hostname + ':' +  port + '/' );
+	console.log( 'Servidor Ejecutándose en http://' + hostname + ':' +  port + '/' );
 });
